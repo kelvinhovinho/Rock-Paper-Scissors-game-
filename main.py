81ca39d1447340e3c16b1ef03ee030493250268b
Akelvino <vinho892@gmail.com>
2024-10-25 20:49:44 +0300

    Finished the rock paper scissors game

--- main.py
+++ main.py
@@ -1,1 +1,56 @@
-print("Hello World!!")
+import random
+
+while True:
+    choices = ['rock','paper','scissors']
+
+    computer = random.choice(choices)
+    player=None
+
+    while player not in choices:
+        player =input("rock, paper or scissors?: ").lower()
+
+
+    if player == computer:
+        print(f'computer,{computer}')
+        print(f'player, {player}')
+        print("Tie!")
+
+    elif player == 'rock':
+        if computer == 'paper':
+            print(f'computer,{computer}')
+            print(f'player, {player}')
+            print("You LOSE!")
+    
+        if computer == 'scissors':
+            print(f'computer,{computer}')
+            print(f'player, {player}')
+            print("You WIN!")
+
+    elif player == 'scissors':
+        if computer == 'rock':
+            print(f'computer,{computer}')
+            print(f'player, {player}')
+            print("You LOSE!")
+    
+        if computer == 'paper':
+            print(f'computer,{computer}')
+            print(f'player, {player}')
+            print("You WIN!")
+
+    elif player == 'paper':
+        if computer == 'scissors':
+            print(f'computer,{computer}')
+            print(f'player, {player}')
+            print("You LOSE!")
+    
+        if computer == 'rock':
+            print(f'computer,{computer}')
+            print(f'player, {player}')
+            print("You WIN!")
+
+    play_again = input("Play again? yes/no ").lower()
+
+    if play_again != "yes":
+        break
+
+print("BYE!!")
